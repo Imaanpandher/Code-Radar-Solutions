c4b93553da7062f443a201709837d1a2fea3dcfa
Imaanpandher <imaanjot0@gmail.com>
2025-02-06 17:27:43 +0530

    Solution to Print a Mirrored Right-Angled Triangle Pattern

---
+++ Print a Mirrored Right-Angled Triangle Pattern@@67877e1818d53eb724f435c1/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d";&a);
+    int m=a;
+    for (int i=1;i<=a;j++){
+        for (int j=2;j<=a;j++)
+        {
+            printf(" ");
+        }
+        for(int k=1;k<=a;k++)
+        {
+            printf("*");
+        }
+        printf("\n")
+    }
+    return 0;
+}
